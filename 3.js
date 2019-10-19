dcb34c98e2ebe7fd683857959452d5ff68c83412
jonik000 <56723910+jonik000@users.noreply.github.com>
2019-10-19 14:45:33 +0300

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,10 @@
+var colorArray = ["#5A9C6E", "#A8BF5A", "#FAC46E", "#FAD5BB", "#F2FEFF"]; 
+var i = 0; 
+
+function changeColor(){
+    document.body.style.background = colorArray[i]; 
+    i++;
+    if( i > colorArray.length - 1){
+        i = 0;
+    }
+}
